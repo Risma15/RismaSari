74b8cdd16db52a159c80f5493443fc3b133dde4d
Risma15 <48237561+Risma15@users.noreply.github.com>
2019-03-07 17:22:51 +0700

    Create Hallo.java

---
+++ Hallo.java
@@ -0,0 +1,9 @@
+public class Hallo {
+    
+    public static void main(String[] args) {
+        System.out.println("Selamat Datang di MK Object Oriented Programming \n"
+                + "Nama  : Risma Sari \n"
+                + "NIM   : 17090052 \n"
+                + "Kelas : 4A");
+    }
+}
